e17603d833f494a2f213919c20379e4379e43e3d
Ser <sergejrotanov@MacBook-Pro-Ser.Dlink>
2018-05-21 18:03:21 +0300

    lesson03 добавил контроллер с продуктами

---
+++ table_coder-swage/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  table_coder-swage
+//
+//  Created by Ser on 21.05.2018.
+//  Copyright © 2018 sergio_rotanov. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+      
+}
---
+++ table_coder-swage/View/ProductCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProductCellCollectionViewCell.swift
+//  table_coder-swage
+//
+//  Created by Ser on 21.05.2018.
+//  Copyright © 2018 sergio_rotanov. All rights reserved.
+//
+
+import UIKit
+
+class ProductCellCollectionViewCell: UICollectionViewCell {
+    
+}
